5b9ed1535aca22aa70759b3f7b01357ad409a16c
nhuhc <nhuhc@viettel.com.vn>
2024-03-07 22:11:28 +0700

    update cau chuc

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Em';
 const giftUrl = 'https://www.facebook.com/phanthanh.huyen.393';
 const eventName = 'Chúc Mừng 8/3';
 const titleCard = 'Phan Huyền';
-const contentCard = 'Chúc Huyền có ngày 8/3 vui vẻ, tràn ngập niềm vui và hạnh phúc nhé.';
+const contentCard = 'Huyền à, em chính là thiên thần xinh đẹp nhất trong trái tim anh. Chúc em của anh ngày 8/3 thật vui vẻ, cảm ơn em nhé.';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
