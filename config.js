ab52fe789c27505085f8e375bb0a17ce74ea6a41
nhuhc <nhuhc@viettel.com.vn>
2024-03-07 21:51:31 +0700

    nhuhc push project

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/phanthanh.huyen.393';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Phan Huyền';
+const contentCard = 'Chúc Huyền có ngày 8/3 vui vẻ, tràn ngập niềm vui và hạnh phúc nhé.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'huyen1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
